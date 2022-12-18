14b06602c66f00506fee8c802f4c5b5f411c2e43
James Song <j298song@uwaterloo.ca>
2022-12-18 06:11:08 -0500

    readded client files

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+// import InputTodos from "./components/InputTodos"
+// import ListTodos from "./components/ListTodos"
+import ApiTest from "./components/ApiTest"
+
+function App() {
+  return (
+    <div >
+      <ApiTest></ApiTest>
+    </div>
+  )
+}
+
+export default App
